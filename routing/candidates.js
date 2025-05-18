9445f2a0ad446d2890ed456246a618ddcc4bec58
dataemperor <155064611+dataemperor@users.noreply.github.com>
2025-05-19 00:05:55 +0530

    corrected spelling error for candidate

--- routing/candidates.js
+++ routing/candidates.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Candidate = require("../Candidate");
+const Candidate = require("../candidate");
 
 // data storaged in memory
 let memoryCandidates = [];
